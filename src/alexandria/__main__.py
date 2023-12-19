b6e538eb8d29e0b415c63c39bbbfb03801c57a33
Olin Gallet <olindgallet@olingallet.com>
2023-12-18 20:37:45 -0600

    Massive Revision

--- src/alexandria/__main__.py
+++ src/alexandria/__main__.py
@@ -1,25 +1,11 @@
 #!/usr/bin/env python3
 
 # Author: Olin Gallet
-# Date:  24/1/2023
+# Date:  18/12/2023
 import sys
 
-from .dbutil.dbconnector import DBConnector
-from .dbutil.createdbcommand import CreateDBCommand
-from .dbutil.createtablecommand import CreateTableCommand
-from .dbutil.insertrecordcommand import InsertRecordCommand
-
 def execute():
-    connector = DBConnector('alexandria')
-    cursor = connector.get_cursor()
-    #CreateTableCommand('movies').execute(cursor)
-    InsertRecordCommand('movies', 'Suspiria').execute(cursor)
-    cursor.close()
-    connector.close()
-    #cur.execute("SELECT * FROM pg_database;")
-    #cur.execute('SELECT table_name FROM information_schema.tables WHERE table_schema = \'public\';')
-    #cur.close()
-    #con.close()
+    pass
     
 if __name__ == '__main__':
     execute()
