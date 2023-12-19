3b67d6397a7dcb56285a182e13cd9db0fa9e2850
Olin Gallet <olindgallet@olingallet.com>
2023-12-18 22:20:46 -0600

    Runs module with Waitress

--- src/alexandria/__main__.py
+++ src/alexandria/__main__.py
@@ -2,10 +2,41 @@
 
 # Author: Olin Gallet
 # Date:  18/12/2023
-import sys
+from flask import Flask
+from flask import jsonify
+from time import gmtime
+from time import strftime
+from waitress import serve
+
+app = Flask(__name__)
+
+@app.route("/", methods=['GET'])
+def server_stats():
+    return jsonify({"time": strftime("%H:%M:%S", gmtime()), "date": strftime("%Y-%m-%d")}), 200
+
+@app.route("/listmusic", methods=['GET'])
+def list_music():
+    return jsonify({"song": "Dab on da Haters", "artist": "Lil og"}), 200
+
+@app.route("/listmovies", methods=['GET'])
+def list_movies():
+    return jsonify({"title": "Gone with the Wind"}), 200
+
+@app.route("/listanime", methods=['GET'])
+def list_anime():
+    return jsonify({"title": "Gone with the Wind"}), 200
+
+@app.route("/listbooks", methods=['GET'])
+def list_books():
+    return jsonify({"title": "Gone with the Wind"}), 200
+
+@app.route("/listmanga", methods=['GET'])
+def list_manga():
+    return jsonify({"title": "Gone with the Wind"}), 200
 
 def execute():
-    pass
-    
-if __name__ == '__main__':
+    serve(app, host='127.0.0.1', port=8080);
+
+if __name__ == "__main__":
     execute()
+    
