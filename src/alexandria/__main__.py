b2c311117be625df308d39d67bd070bb7dd5c5a0
Olin Gallet <olindgallet@olingallet.com>
2024-01-28 04:09:21 -0600

    Added code to load data from json files.

--- src/alexandria/__main__.py
+++ src/alexandria/__main__.py
@@ -4,25 +4,36 @@
 # Date:  18/12/2023
 from flask import Flask
 from flask import jsonify
-from datetime import datetime
+from flask import Response
+from flask_caching import Cache
 from time import strftime
 from waitress import serve
-from plumbum import local, colors
-from dotenv import load_dotenv, find_dotenv
+from plumbum import local
+from plumbum import colors
+from dotenv import load_dotenv
+from dotenv import find_dotenv
 import sys
-import os, os.path
+import os
+import os.path
 import json
 from json.decoder import JSONDecodeError
 
 _MOVIE_PATH_KEY   = 'MOVIE_PATH'
 _TV_SHOW_PATH_KEY = 'TV_SHOW_PATH'
 
+_NO_MOVIE_DB_RESPONSE = Response("{'message':'No movie db available.'}", status=404, mimetype='application/json')
+_NO_TV_SHOW_DB_RESPONSE = Response("{'message':'No TV show db available.'}", status=404, mimetype='application/json')
+
 _ALERT_COLOR = colors.orchid
 _MESSAGE_COLOR = colors.blue
 _SUCCESS_COLOR = colors.yellow
 _ERROR_COLOR = colors.red
 
+_CACHE_TIMEOUT = 43200 # 60 seconds x 60 minutes x 12 hours
+
 app = Flask(__name__)
+app.config['CACHE_TYPE'] = 'simple'
+cache = Cache(app)
 
 @app.route("/", methods=['GET'])
 def server_stats():
@@ -33,8 +44,34 @@ def list_music():
     return jsonify({"song": "Dab on da Haters", "artist": "Lil og"}), 200
 
 @app.route("/listmovies", methods=['GET'])
+@cache.memoize(_CACHE_TIMEOUT)
 def list_movies():
-    return jsonify({"title": "Gone with the Wind"}), 200
+    response = _NO_MOVIE_DB_RESPONSE
+    if _MOVIE_PATH_KEY in os.environ and os.path.exists(os.environ[_MOVIE_PATH_KEY]):
+        try:
+            with open(os.environ[_MOVIE_PATH_KEY], 'r') as movies_file:
+                movie_data = json.dumps(json.load(movies_file))
+                response = Response(response=movie_data, status=200, mimetype='application/json')
+        except IOError:
+            pass
+        except JSONDecodeError:
+            pass
+    return response
+
+@app.route("/listtvshows", methods=['GET'])
+@cache.memoize(_CACHE_TIMEOUT)
+def list_tv_shows():
+    response = _NO_TV_SHOW_DB_RESPONSE
+    if _MOVIE_PATH_KEY in os.environ and os.path.exists(os.environ[_TV_SHOW_PATH_KEY]):
+        try:
+            with open(os.environ[_TV_SHOW_PATH_KEY], 'r') as tv_shows_file:
+                tv_shows_data = json.dumps(json.load(tv_shows_file))
+                response = Response(response=tv_shows_data, status=200, mimetype='application/json')
+        except IOError:
+            pass
+        except JSONDecodeError:
+            pass
+    return response
     
 @app.route("/listanime", methods=['GET'])
 def list_anime():
@@ -55,18 +92,10 @@ def _parse_env(location: str):
     '''
     load_dotenv(find_dotenv(location))
     print(_ALERT_COLOR | '[--LOADING FILE DATA--]')
-    if _MOVIE_PATH_KEY in os.environ:
+    if _MOVIE_PATH_KEY in os.environ and os.path.exists(os.environ[_MOVIE_PATH_KEY]):
         print()
-        print(_MESSAGE_COLOR | f'Searching for: {os.environ[_MOVIE_PATH_KEY]}')
-        try:
-            with open(os.environ[_MOVIE_PATH_KEY], 'r') as movies_file:
-                _movie_data = json.load(movies_file)
-                print(_SUCCESS_COLOR | '[MOVIE DATA SUCCESSFULLY LOADED]')
-        except IOError:
-            print(_ERROR_COLOR | f'Movie file not found: {os.environ[_MOVIE_PATH_KEY]}')
-            pass
-        except JSONDecodeError:
-            print(_ERROR_COLOR | 'Movie file not in proper JSON format')
+        print(_MESSAGE_COLOR | f'Movie DB found: {os.environ[_MOVIE_PATH_KEY]}')
+        
     if _TV_SHOW_PATH_KEY in os.environ:
         print()
         print(_MESSAGE_COLOR | f'Searching for: {os.environ[_TV_SHOW_PATH_KEY]}')
